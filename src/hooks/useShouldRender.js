35220fcf99e2719b6d8e9f9c6abfa456cecd6e87
ComradeUa <49310103+ComradeUa@users.noreply.github.com>
2025-03-05 08:59:19 +0200

    Delete src/hooks/useShouldRender.js

--- src/hooks/useShouldRender.js
+++
@@ -1,9 +0,0 @@
-import { useCallback, useState } from 'react';
-const useShouldRender = () => {
-  const [shouldRenderInput, setShouldRenderInput] = useState(true);
-  const handleToggleRender = useCallback(() => {
-    setShouldRenderInput((prev) => !prev);
-  }, []);
-  return { shouldRenderInput, setShouldRenderInput, handleToggleRender };
-};
-export default useShouldRender;
